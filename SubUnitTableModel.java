195ada12282da07ce97783041cceb8240f1cc1bc
fosztomatyas <32310039+fosztomatyas@users.noreply.github.com>
2018-02-23 12:01:06 +0200

    yeye nem megy semi

--- SubUnitTableModel.java
+++ SubUnitTableModel.java
@@ -4,6 +4,9 @@ public class SubUnitTableModel extends DefaultTableModel {
     //https://stackoverflow.com/questions/12776021/how-to-make-jtable-cell-editable-noneditable-dynamically
 
     private boolean[][] editable_cells; // 2d array to represent rows and columns
+    private int editedRow;
+    private int editedColumn;
+
 
     public SubUnitTableModel(String[][] data, String[] columns) { // constructor
         super(data, columns);
@@ -22,9 +25,24 @@ public class SubUnitTableModel extends DefaultTableModel {
     public boolean isCellEditable(int row, int column) { // custom isCellEditable function
         return this.editable_cells[row][column];
     }
+    @Override
+    public void setValueAt(Object value, int row, int col){
+        super.setValueAt(value,row,col);
+        editedRow = row;
+        editedColumn = col;
+    }
 
     public void setCellEditable(int row, int col, boolean value) {
         this.editable_cells[row][col] = value; // set cell true/false
         //this.fireTableCellUpdated(row, col);//ha ezt kiszedem mukodik. nagyon szegyellem magam
+    }
+
+
+    public int getEditedRow() {
+        return editedRow;
+    }
+
+    public int getEditedColumn() {
+        return editedColumn;
     }
 }
