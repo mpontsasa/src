b4660846952ba4ecfba7aed2cb405df9bbe203f7
mpontsasa <mpontsasa@users.noreply.github.com>
2018-02-24 09:34:40 +0200

    na

--- ScheduleModel.java
+++ ScheduleModel.java
@@ -1,20 +1,4 @@
 // az orarend tablazathoz hasznaljuk
 public class ScheduleModel implements  SuperModel{
-
-    private int n = 0;
-
-    public void loadLine(String line){
-        System.out.println("schedule got:" + line);
-    }
-
-
-    public String saveLine(){
-        if(n < 3){
-            n++;
-            return "scheduleeeetestsaveline";
-        }
-
-        else
-            return Finals.END_OF_PROJECT;
-    }
+    
 }
